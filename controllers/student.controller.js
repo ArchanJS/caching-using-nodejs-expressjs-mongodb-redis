c72a879c3f006e4401fa04f1f1f08bed01f980db
Archan Banerjee <archanbanerjee70@gmail.com>
2022-05-15 10:47:13 +0530

    final commit

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -19,21 +19,34 @@ exports.createStudent=async(req,res)=>{
 // Get all students
 exports.getAllStudents=async(req,res)=>{
     try {
-        await redisClient.connect();
-        let result=await redisClient.get('students');
-        if(result==null){
-            const students=await Student.find();
-            redisClient.setEx('students',process.env.EXPIRES,JSON.stringify(students)); //EXPIRES=3600
-            redisClient.quit();
-            res.status(200).send(students);
-        }
-        else{
-            result=JSON.parse(result);
-            redisClient.quit();
-            res.status(200).send(result);
-        }
+        const result=await getOrSetCache('students',async()=>{
+            try {
+                const students=await Student.find();
+                return students;
+            } catch (error) {
+                throw new Error("Something went wrong while caching!");
+            }
+        });
+        res.status(200).send(result);
     } catch (error) {
         console.log(error);
         res.status(500).json({error:"Something went wrong!"});
     }
+}
+
+const getOrSetCache=(key,cb)=>{
+    return new Promise(async(resolve,reject)=>{
+        await redisClient.connect();
+        const data=await redisClient.get(key);
+        if(data!=null){
+            redisClient.quit();
+            return resolve(JSON.parse(data));
+        }
+        else{
+            const students=await cb();
+            redisClient.setEx(key,process.env.EXPIRES,JSON.stringify(students)); //EXPIRES=3600
+            redisClient.quit();
+            resolve(students);
+        }
+    })
 }
